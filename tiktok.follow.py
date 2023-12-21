8d35a46257b8f62771cb77cbf29e80adaff28c1c
Mohamad Supangat <hantamkoding@gmail.com>
2023-12-21 14:16:47 +0700

    fix: auto follow tiktok

--- tiktok.follow.py
+++ tiktok.follow.py
@@ -1,10 +1,31 @@
-# mancing
 import time
 from autoclick import helpers
+
+
+total = 0
+imgs = {
+    'follow_button': './imgs/tiktok/follow_button.png'
+}
+
+
+def search_follow_button(ss):
+    return helpers.locateCenterOnImage(imgs['follow_button'], ss)
+
+def scroll_page():
+    return helpers.d.swipe(292, 1250, 292, 163, 0.5)
 
 while True:
     ss = helpers.screenshot()
 
-    time.sleep(4)
+    position = search_follow_button(ss)
+    if position:
+        helpers.tap(*position)
+        total += 1
+    else:
+        print(f"total: {total}")
+        scroll_page()
+
+    # break
+    time.sleep(2)
 
 
